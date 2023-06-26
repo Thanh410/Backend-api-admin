e8a794f8b6cc8bb9e2e8ccb0e96f7006ac695a99
Thanh410 <59430898+Thanh410@users.noreply.github.com>
2023-06-26 21:26:50 +0700

    Update connect.js

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -5,7 +5,7 @@ async function Connect() {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     console.log("Connect Successfully");
   } catch (err) {
