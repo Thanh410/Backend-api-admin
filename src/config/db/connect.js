69571678ac3cc7c47b25ae523a3689fe758876ad
Thanh410 <59430898+Thanh410@users.noreply.github.com>
2023-06-26 21:51:18 +0700

    Update connect.js

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 async function Connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Booking_App", {
-      useNewUrlParser: true
-      
-    });
+    await mongoose.connect("mongodb://127.0.0.1:27017/Booking_App");
     console.log("Connect Successfully");
   } catch (err) {
     console.log(err);
