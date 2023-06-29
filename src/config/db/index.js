382e94c3655b4bf241b06b94bf5043c2c22963a8
Thanh410 <59430898+Thanh410@users.noreply.github.com>
2023-06-29 19:54:20 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,11 +3,8 @@ require("dotenv").config();
 
 async function Connect() {
   try {
-<<<<<<< HEAD
     await mongoose.connect(process.env.MONGODB, {
-=======
-    await mongoose.connect(process.env.MONGODB || "mongodb://0.0.0.0:27017/Booking_App", {
->>>>>>> cb14279010f0c3e7ec72b2410c0e00983a1672c8
+
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
